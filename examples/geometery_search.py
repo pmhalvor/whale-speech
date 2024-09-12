e3495d4b49d5b33441d5706b51dd9fceaba7c5c4
Per Morten Halvorsen <per.morten.halvorsen@schibsted.com>
2024-09-12 23:13:13 +0200

    add exmaple geo search

---
+++ examples/geometery_search.py
@@ -0,0 +1,10 @@
+import happywhale.happywhale as hw
+
+# geometery search for a time-frame
+geometry_file = "data/geo/monterey_bay_50kmr.geojson"
+start = "2024-09-01"
+end = "2024-09-02"
+export = "data/sightings/monterey_bay_50kmr_2024-09-01_2024-09-02.geojson"
+species = "humpback_whale"
+
+hw.geometry_search(geometry_file, start, end, export, species)
