5c7a8dc38c2a42b370093f752a37d53341795a49
Per Halvorsen <pmchalvorsen@gmail.com>
2024-09-29 15:03:01 +0200

    build config dir

--- src/config.py
+++ src/config.py
@@ -7,8 +7,21 @@ import yaml
 
 CONFIG_FILE_PATH = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 
-    'config.yaml'
+    'config/common.yaml'
 )
+ENV = os.environ.get('ENV', "local")
+if ENV == "local":
+    EXTRA_CONFIG_FILE_PATH = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 
+        'config/local.yaml'
+    )
+elif ENV == "gcp":
+    EXTRA_CONFIG_FILE_PATH = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 
+        'config/gcp.yaml'
+    )
+else:
+    raise ValueError(f"Invalid ENV: {ENV}")
 
 
 # Function to load defaults from config file
@@ -27,6 +40,17 @@ def parse_args(config):
         parser.add_argument(f'--{key}', type=type(value), default=value, required=False)
 
     return parser.parse_known_args()
+
+
+def append_values(config, extra_config):
+    """
+    Append values to the config dictionary.
+    """
+    for stage in config.keys():
+        if stage in extra_config:
+            config[stage].update(extra_config[stage])
+
+    return config
 
 
 def update_config(config):
@@ -54,15 +78,22 @@ def dict_to_namespace(d):
     return SimpleNamespace(**d)
 
 
-
-def load_pipeline_config(config_file = CONFIG_FILE_PATH):
+def load_pipeline_config(
+        config_file = CONFIG_FILE_PATH,
+        extra_config_file = EXTRA_CONFIG_FILE_PATH
+    ):
     """
     Load config file and override with command-line arguments.
 
     NOTE: If conflicting parameter names, both cases 
     will be updated with value from command-line arg.
     """
+    # files
     config = read_config(config_file)["pipeline"]
+    extra_config = read_config(extra_config_file)["pipeline"]
+    config = append_values(config, extra_config)
+
+    # command-line arguments
     config = update_config(config)
 
     return dict_to_namespace(config)
