258b596e52182dd6ee612dd393a2691a696d4788
Per Halvorsen <pmchalvorsen@gmail.com>
2024-10-19 10:02:54 +0200

    test limit on model server -> max batch duration = 60 sec

--- examples/test_server.py
+++ examples/test_server.py
@@ -7,9 +7,12 @@ inference_url = os.environ.get("INFERENCE_URL", "0.0.0.0:5000/predict")
 data = {
     "key": "encouter1",
     "batch": [
-        [0.3745401203632355], [0.9507142901420593], [0.7319939136505127], [0.5986585021018982],
-    ]*10_000, 
+        [0.3745401203632355], [0.9507142901420593], [0.7319939136505127], [0.5986585021018982], [0.15601864044243652], [0.15599452033620265]
+    ]*10_000*10*1, # (6 samples * 10_000 = 6 seconds )* 10 = 60 seconds 
 }
 
 response = requests.post(inference_url, json=data)
-print(response.json())
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(response.text)
